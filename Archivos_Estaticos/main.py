723d91a3e1b1e9e3152c23e28595cc76e3d502ba
jhasmany-jhunnior <jhasmany_jhunnior@outlook.com>
2022-01-18 21:42:27 -0400

    9.- Curso Flask - Archivos estáticos

---
+++ Archivos_Estaticos/main.py
@@ -0,0 +1,13 @@
+from turtle import title
+from flask import Flask
+from flask import render_template
+
+app =  Flask(__name__)
+
+@app.route('/')
+def index():
+    title = "Curso Flask"
+    return render_template('index.html', title = title)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
